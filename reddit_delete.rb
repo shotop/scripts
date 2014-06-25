8bb1493948e5c6184feae9edc924c8a2b6dd97b1
Samuel Hotop <samuelhotop@Samuels-iMac.local>
2014-06-24 19:25:59 -0500

    shotop: script to delete reddit history using snooby gem

---
+++ reddit_delete.rb
@@ -0,0 +1,15 @@
+require 'snooby'
+
+USERNAME = "xxxxx"
+PASSWORD = "xxxxx"
+
+reddit = Snooby::Client.new('me, v1.0')
+reddit.authorize!(USERNAME, PASSWORD)
+
+comments = reddit.user(USERNAME).comments(1000)
+
+comments.each do |comment|
+	comment.delete
+end
+
+
